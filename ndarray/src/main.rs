4285e5e087d79c2aad79e3eb70689031020b51ad
M Rifqan Zalbina <124742008+rifqanzalbina@users.noreply.github.com>
2023-09-04 12:49:15 +0700

    Update main.rs

--- ndarray/src/main.rs
+++ ndarray/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use polars::prelude::*;
+
+fn main() -> Result<()> {
+    let file_path = "path/to/your/csvfile.csv";
+    let df = CsvReader::from_path(file_path)?
+        .infer_schema(Some(16))
+        .finish()?;
+    
+    println!("{:?}", df);
+    Ok(())
 }
